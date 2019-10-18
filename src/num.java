2fa2c0f83d5a907390167886a45e8e25b9cf7681
Nathan Dilla <91088509@mvusd.net>
2019-10-18 13:32:26 -0700

    done.

---
+++ src/num.java
@@ -0,0 +1,19 @@
+public class num {
+    private int value;
+    public num (int update)
+    {
+        value = update;
+    }
+    public int getValue()
+    {
+        return value;
+    }
+    public void setValue(int update)
+    {
+        value = update;
+    }
+    public String toString()
+    {
+        return value + "";
+    }
+}
